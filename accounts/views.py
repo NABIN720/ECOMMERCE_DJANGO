398c5d499442aa2264da4acf190f4cc7fec6260a
NABIN720 <nabinbhattarai720@gmail.com>
2025-05-07 12:08:05 +0545

    added activation email

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,18 @@
-from django.shortcuts import render, redirect
-from django.http import HttpResponse
-from django.contrib.auth.models import User
+from django.conf import settings
 from django.contrib import messages
 from django.contrib.auth import authenticate, login
-from django.urls import reverse
-from django.core.validators import validate_email
+from django.contrib.auth.models import User
+from django.contrib.auth.tokens import default_token_generator
+from django.contrib.sites.shortcuts import get_current_site
 from django.core.exceptions import ValidationError
+from django.core.mail import send_mail
+from django.core.validators import validate_email
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.template.loader import get_template,render_to_string
+from django.urls import reverse
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes
 # Create your views here.
 
 def signup_view(request):
@@ -30,13 +37,36 @@ def signup_view(request):
         if User.objects.filter(username=email).exists():
             messages.error(request,"User already exists, please try with different email!!!")
             return redirect("signup_view")
-        
 
         userObj = User.objects.create_user(username=email, email=email,password=pass1)
         userObj.first_name = fname
         userObj.last_name = lname
+        userObj.is_active = False
 
         userObj.save()
+
+        # token and uid 
+        htmly = get_template()
+        token = default_token_generator.make_token(user)
+        uid = urlsafe_base64_encode(force_bytes(userObj.pk))
+        current_site = get_current_site(request)
+        activation_link = f"http://{current_site.domain}/activate/{uid}/{token}"
+
+        # html_messages
+        html_message = render_to_string('activation_email.html', {
+            'user':userObj,
+            'activation_link':activation_link,
+        })
+
+
+        send_mail(
+            'activate_your_account',
+            html_message,
+            settings.DEFAULT_FROM_EMAIL,
+            [userObj.email],
+            fail_silently = False,
+
+        )
 
         messages.success(request,"Your Account has been sucessfully created")
 
@@ -76,3 +106,18 @@ def logout_view(request):
     return HttpResponse("from logout")
 
 
+def activate(request, uidb64, token):
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = User.objects.get(pk=uid)
+
+    except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+
+    if user and default_token_generator.check_token(user, token):
+        user.is_active = True
+        user.save()
+        return redirect('dokan/index.html')
+    else:
+        messages.error(request, 'Activation failed please signup again!!!')
+        return redirect('signup_view')
--- ecom/settings.py
+++ ecom/settings.py
@@ -12,6 +12,17 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+from .info import *
+
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = EMAIL_USE_TLS
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+EMAIL_HOST = EMAIL_HOST
+EMAIL_PORT = EMAIL_PORT
+EMAIL_HOST_USER = EMAIL_HOST_USER
+EMAIL_HOST_PASSWORD = EMAIL_HOST_PASSWORD
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -138,3 +149,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'dokan/index'  # Redirect after login
 LOGOUT_REDIRECT_URL = '/accounts/signup/'  # Redirect after logout
+
+
